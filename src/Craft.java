a3737ce4dd26144c9c4a63734d5e409b470bc654
Michał <bukaniex@gmail.com>
2017-02-20 20:45:20 +0100

    class representing spaceship created

--- src/Craft.java
+++ src/Craft.java
@@ -1,7 +1,82 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 /**
  * Created by user_name on 20.02.2017.
  */
 public class Craft  {
     private int dx;
     private int dy;
+    private int x;
+    private int y;
+    private Image image;
+
+    public Craft(){
+        initCraft();
+    }
+
+    private void initCraft(){
+        ImageIcon ii = new ImageIcon("craft.png");
+        image = ii.getImage();
+        x = 40;
+        y = 60;
+    }
+
+    public void move(){
+        x += dx;
+        y += dy;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public Image getImage(){
+        return image;
+    }
+
+    public void keyPressed(KeyEvent e){
+        int key = e.getKeyCode();
+
+        if(key == KeyEvent.VK_LEFT){
+            dx = -1;
+        }
+
+        if(key == KeyEvent.VK_RIGHT){
+            dx = 1;
+        }
+
+        if(key == KeyEvent.VK_UP){
+            dy = -1;
+        }
+
+        if(key == KeyEvent.VK_DOWN){
+            dy = 1;
+        }
+    }
+
+    public void keyReleased(KeyEvent e){
+        int key = e.getKeyCode();
+
+        if(key == KeyEvent.VK_LEFT){
+            dx = 0;
+        }
+
+        if(key == KeyEvent.VK_RIGHT){
+            dx = 0;
+        }
+
+        if(key == KeyEvent.VK_UP){
+            dy = 0;
+        }
+
+        if(key == KeyEvent.VK_DOWN){
+            dy = 0;
+        }
+    }
 }
